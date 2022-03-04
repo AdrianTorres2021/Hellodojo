a609cc95a0f6747744ca03dd0207b6042abad497
Adrian Torres <adrianluistorres2@gmail.com>
2022-03-04 14:47:57 -0500

    Hellodo finished

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+
+function App() {
+  return (
+    <>
+  <h1>Hello Dojo!</h1> 
+  <h2>Things I need to do:</h2>
+  <li>Learn React</li>
+  <li>Climb Mt. Everest</li>
+  <li>Run a marathon</li>
+  <li>Feed the dogs</li>
+    </>
+    );
+}
+
+export default App;
